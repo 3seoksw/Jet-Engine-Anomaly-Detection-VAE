cdf4c1d64e268fc76b9c8dbb8e8a71ce13099ec5
WooSeok Kim <3suksw@gmail.com>
2026-03-31 00:40:00 -0400

    add: manual seed numbers to control exp

--- src/train.py
+++ src/train.py
@@ -1,15 +1,20 @@
 import hydra
 import torch
+import numpy as np
 from omegaconf import DictConfig
 from hydra.core.hydra_config import HydraConfig
 
 from data_module.cmapss_dataloader import FullDataLoader
+from models.vae import VAE
 from models.rnn_vae import RNN_VAE
 from trainer.trainer import Trainer
 
 
 @hydra.main(version_base=None, config_path="../conf", config_name="config")
 def main(cfg: DictConfig):
+    torch.manual_seed(100)
+    np.random.seed(100)
+
     if torch.cuda.is_available():
         device = "cuda"
     elif torch.mps.is_available():
@@ -23,12 +28,23 @@ def main(cfg: DictConfig):
         window=cfg.data.window,
         batch_size=cfg.data.batch_size,
     )
-    model = RNN_VAE(
-        input_dim=cfg.model.input_dim,
-        d_model=cfg.model.d_model,
-        n_layers=1,
-        latent_dim=cfg.model.latent_dim,
-    )
+    input_dim = d_loader.get_input_dim()
+
+    if cfg.data.window == 1:
+        model = VAE(
+            input_dim=input_dim,
+            d_model=cfg.model.d_model,
+            n_layers=1,
+            latent_dim=cfg.model.latent_dim,
+        )
+    else:
+        model = RNN_VAE(
+            input_dim=input_dim,
+            d_model=cfg.model.d_model,
+            n_layers=1,
+            latent_dim=cfg.model.latent_dim,
+        )
+    print(sum(p.numel() for p in model.parameters() if p.requires_grad))
 
     log_dir = HydraConfig.get().runtime.output_dir
     trainer = Trainer(
@@ -39,8 +55,10 @@ def main(cfg: DictConfig):
         beta=cfg.train.beta,
         gamma=cfg.train.gamma,
         log_dir=log_dir,
+        n_epochs=cfg.train.epochs,
     )
-    trainer.fit(cfg.train.epochs)
+    trainer.fit()
+    trainer.test()
 
 
 if __name__ == "__main__":
